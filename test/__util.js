b8f54326e80c27d78764faa078681105ba0329e8
Kevin Tucker <kwtucker4@gmail.com>
2016-06-22 20:12:30 -0400

    added tests for the util.versionbump

--- test/__util.js
+++ test/__util.js
@@ -9,6 +9,8 @@ const util = require('../src/util');
 const sinon = require('sinon');
 const fs = require('fs');
 const rimraf = require('rimraf');
+const pkgDotJson = require('../package.json');
+
 
 // Setting up global variables
 const path = './log/';
@@ -79,4 +81,74 @@ describe('Utility Tool Testing', () => {
       if (err) throw err;
     });
   }); // end export logs test
+});
+
+
+describe('Version Bump Util: ', () => {
+  const originalVersion = pkgDotJson.version;
+  // Split the string at the dot
+  let newVersion;
+  let oldTagValues;
+  let tagValues;
+  after(() => {
+    // Reset the old version
+    pkgDotJson.version = originalVersion;
+  });
+
+  it('Should increment major and zero minor and patch', () => {
+    // This will use the major bump of debug tool and set to var
+    newVersion = util.versionbump(pkgDotJson.version, 'major');
+    // Split into array using the dot
+    tagValues = newVersion.split('.');
+    oldTagValues = pkgDotJson.version.split('.');
+    // Turn all values to int
+    for (let i = 0; i < tagValues.length; i++) {
+      tagValues[i] = parseInt(tagValues[i], 10);
+    }
+    for (let o = 0; o < oldTagValues.length; o++) {
+      oldTagValues[o] = parseInt(oldTagValues[o], 10);
+    }
+    // Test that each position is what it should be.
+    expect(tagValues[0]).to.equal(oldTagValues[0] + 1);
+    expect(tagValues[1]).to.equal(0);
+    expect(tagValues[2]).to.equal(0);
+  });
+
+  it('Should increment minor and zero patch', () => {
+    // This will use the major bump of debug tool and set to var
+    newVersion = util.versionbump(pkgDotJson.version, 'minor');
+    // Split into array using the dot
+    tagValues = newVersion.split('.');
+    oldTagValues = pkgDotJson.version.split('.');
+    // Turn all values to int
+    for (let i = 0; i < tagValues.length; i++) {
+      tagValues[i] = parseInt(tagValues[i], 10);
+    }
+    for (let o = 0; o < oldTagValues.length; o++) {
+      oldTagValues[o] = parseInt(oldTagValues[o], 10);
+    }
+    // Test that each position is what it should be.
+    expect(tagValues[0]).to.equal(oldTagValues[0]);
+    expect(tagValues[1]).to.equal(oldTagValues[1] + 1);
+    expect(tagValues[2]).to.equal(0);
+  });
+
+  it('Should increment patch', () => {
+    // This will use the major bump of debug tool and set to var
+    newVersion = util.versionbump(pkgDotJson.version, 'patch');
+    // Split into array using the dot
+    tagValues = newVersion.split('.');
+    oldTagValues = pkgDotJson.version.split('.');
+    // Turn all values to int
+    for (let i = 0; i < tagValues.length; i++) {
+      tagValues[i] = parseInt(tagValues[i], 10);
+    }
+    for (let o = 0; o < oldTagValues.length; o++) {
+      oldTagValues[o] = parseInt(oldTagValues[o], 10);
+    }
+    // Test that each position is what it should be.
+    expect(tagValues[0]).to.equal(oldTagValues[0]);
+    expect(tagValues[1]).to.equal(oldTagValues[1]);
+    expect(tagValues[2]).to.equal(oldTagValues[2] + 1);
+  });
 });
