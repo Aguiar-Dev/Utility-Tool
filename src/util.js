e8055522bc4b94143bc3c2884c832ed642800bf1
Kevin Tucker <kwtucker4@gmail.com>
2016-06-22 20:11:55 -0400

    version bump util

--- src/util.js
+++ src/util.js
@@ -75,3 +75,45 @@ exports.debug = (data) => {
   }
   return info;
 };
+
+exports.versionbump = (currentVersion, tag) => {
+  let newVersion;
+  // Split the string at the dot
+  const tagValues = currentVersion.split('.');
+  // Loop through the split array
+  for (let i = 0; i <= tagValues.length; i++) {
+    parseInt(tagValues[i], 10);
+  }
+  // Varify that the tag is either major minor or patch.
+  if (tag === 'major' || tag === 'minor' || tag === 'patch') {
+    if (tag === 'major') {
+      // Increase major and zero out minor and patch.
+      // Then turn everything to a sting and join with a dot.
+      tagValues[0]++;
+      tagValues[0].toString();
+      tagValues[1] = '0';
+      tagValues[2] = '0';
+      newVersion = tagValues.join('.');
+    } else if (tag === 'minor') {
+      // increase minor and zero patch.
+      // Then turn everything to a sting and join with a dot.
+      tagValues[0].toString();
+      tagValues[1]++;
+      tagValues[1].toString();
+      tagValues[2] = '0';
+      newVersion = tagValues.join('.');
+    } else if (tag === 'patch') {
+      // increase patch.
+      // Then turn everything to a sting and join with a dot.
+      tagValues[0].toString();
+      tagValues[1].toString();
+      tagValues[2]++;
+      tagValues[2].toString();
+      newVersion = tagValues.join('.');
+    }
+  } else {
+    // return an error string to throw error in console with gulp
+    newVersion = 'error';
+  }
+  return newVersion;
+};
